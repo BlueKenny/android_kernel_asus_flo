8d8d3cde7169cffa08209a2c9ba3e6895810abe1
Josh Chasky <jchasky@gmail.com>
2015-12-08 21:13:32 -0500

    Flo: Misc fixes
    Revert "cpufreq: create a helper function to get CPU utilization"
    
    This reverts commit 67680d31439be5693fd4307c01194e37b6545d96.
    
    Revert "cpufreq: Introduce new relation for freq selection"
    
    This reverts commit 406cbd75fb843a3943c776a09054463a2d13d59f.
    
    Revert "cpufreq: add generic get_cpuidle_time to the cpufreq headers"
    
    This reverts commit de0b54d57e43c7fed4a968187c41fdaadeafd07e.
    
    Revert "Flo: Regen defconfig"
    
    This reverts commit 60c9c7e895957b7aa139f02cd33b031cc17f1f34.
    
    Revert "Flo: Add blu_active CPU gov & blu_plug CPU hotplug"
    
    This reverts commit 632885ae549f46a6806cfe9ef912bf568f3e4b28.
    
    Change-Id: I6a324fc8a544979543cbd1690ed3c79cfa48da61

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -218,7 +218,6 @@ void unlock_policy_rwsem_write(int cpu);
 
 #define CPUFREQ_RELATION_L 0  /* lowest frequency at or above target */
 #define CPUFREQ_RELATION_H 1  /* highest frequency below or at target */
-#define CPUFREQ_RELATION_C 2  /* closest frequency to target */
 
 struct freq_attr;
 
@@ -322,7 +321,6 @@ __ATTR(_name, 0644, show_##_name, store_##_name)
 /*********************************************************************
  *                        CPUFREQ 2.6. INTERFACE                     *
  *********************************************************************/
-u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy);
 int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu);
 int cpufreq_update_policy(unsigned int cpu);
 
@@ -340,7 +338,6 @@ static inline unsigned int cpufreq_get(unsigned int cpu)
 #ifdef CONFIG_CPU_FREQ
 unsigned int cpufreq_quick_get(unsigned int cpu);
 unsigned int cpufreq_quick_get_max(unsigned int cpu);
-unsigned int cpufreq_quick_get_util(unsigned int cpu);
 #else
 static inline unsigned int cpufreq_quick_get(unsigned int cpu)
 {
@@ -391,9 +388,6 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
-extern struct cpufreq_governor cpufreq_gov_blu_active;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_blu_active)
 #endif
 
 
